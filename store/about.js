16f4dc6afa60aa4f77f11c710464d951a8f1cdc6
HyungJun-Yoo <u-jun11@nate.com>
2021-09-21 17:31:10 +0900

    Add nuxt project

---
+++ store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'ITBlue',
+    email: 'hyungjunyoo95@gmail.com',
+    git: 'https://github.com/HyungJun-Yoo',
+    blog: 'https://bluelearn.tistory.com/',
+    image: 'https://raw.githubusercontent.com/HyungJun-Yoo/vue-movie-app/master/src/assets/Blue.png'
+  })
+}
